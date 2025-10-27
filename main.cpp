ba3bea910f758b1569281b086834a9ca7ab42d2a
ioana-caty <caramidaioana28@yahoo.com>
2025-10-28 00:18:57 +0200

    Main in curs de finalizare (alegerea corecta)

--- main.cpp
+++ main.cpp
@@ -5,124 +5,77 @@
 #include "Zbor.h"
 #include "CompanieAeriana.h"
 
+void populareDate(CompanieAeriana& companie) {
+    std::cout << "---POPULARE DATE---";
+
+    Zbor z1("RO123", "Paris", "A12", 5);
+    Zbor z2("RO456", "Londra", "B5", 4);
+    Zbor z3("RO789", "Berlin", "C20", 3);
+
+    Bilet b1("14A", "Economic", 150.0);
+    Bilet b2("1F", "Business", 350.5);
+    b2.aplicaDiscount(10);
+    Bilet b3("22C", "Economic", 120.6);
+
+    Pasager p1("Iustina Caramida", "i.caramida@gmail.com", b1);
+    Pasager p2("Costin Sin", "c.sin@gmail.com", b2);
+    Pasager p3("Tudor Deaconu", "tudor.d@gmail.com", b3);
+
+    z1.adaugaPasager((p1));
+    z1.adaugaPasager((p2));
+    z2.adaugaPasager((p3));
+
+    companie.adaugaZbor(z1);
+    companie.adaugaZbor(z2);
+    companie.adaugaZbor(z3);
+
+    std::cout <<"S-au implementat datele!\n";
+}
+
 int main() {
-    std:: cout <<"SISTEM MANAGEMENT ZBORURI";
+    CompanieAeriana companie("Wizz");
+    populareDate(companie);
 
-    // BILET
-    std:: cout << "\n\nTEST1: Functionalitate Bilete";
-    Bilet b1;
-    Bilet b2("12A", "Economic", 120.50);
-    Bilet b3("15F", "Business", 350.5);
-    std::cout <<"\nBilete create:\n" << b1 << "\n" << b2 << "\n" << b3 << "\n";
+    int optiune;
+    while (true) {
+        std::cout << "\n---MENIU---\n";
+        std::cout << "1. Afiseaza date companie\n";
+        std::cout << "2. Adauga zbor\n";
+        std::cout <<"3. Cauta zbor\n";
+        std::cout << "0. Iesire\n";
+        std::cout <<"Optiune: ";
+        std::cin >> optiune;
 
-    std::cout << "\nLoc b2: " << b2.getLoc() << "\n";
-    b2.setLoc("14B");
-    std::cout << "Loc nou: " << b2.getLoc() << "\n";
+        if (optiune == 0) {
+            break;
+        }
+        if (optiune == 1) {
+            std::cout << "\n" << companie << "\n";
+        }
+        if (optiune == 2) {
+            std::string numar, destinatie, poarta;
+            int cap;
+            std::cout <<"Numar zbor: "; std::cin >> numar;
+            std::cout <<"Destinatie: "; std::cin >> destinatie;
+            std::cout <<"Poarta: "; std::cin >> poarta;
+            std::cout <<"Capacitate: "; std::cin >> cap;
 
-    b3.aplicaDiscount(20);
-    b3.aplicaDiscount(90);
+            Zbor z(numar, destinatie, poarta, cap);
+            companie.adaugaZbor(z);
+            std::cout << "Zbor adaugat cu succes!\n";
+        }
+        if (optiune == 3) {
+            std::string numar;
+            std::cout <<"Numar zbor: "; std::cin >> numar;
 
-    std::cout   << "\nPret final b3: " << std::fixed << std::setprecision(2)
-                << b3.getPretFinal() << " EUR\n";
-
-    Bilet geam("12A", "Economic", 100.3);
-    Bilet culoar("8C", "Economic", 100.1);
-
-    std::cout << "Verificare Locuri la geam: ";
-    std::cout << (geam.isWindowSeat() ? "DA" : "NU");
-    std::cout << (culoar.isWindowSeat() ? " DA" : " NU") << "\n";
-
-
-    // PASAGER
-    std:: cout << "\nTEST2: Functionalitate Pasageri";
-
-    Bilet bilet1("12A", "Economic", 150.7);
-    Bilet bilet2("8F", "Business", 400.5);
-
-    Pasager p1;
-    Pasager p2("Iustina Caramida", "i.caramida@yahoo.com", bilet1);
-    Pasager p3("Ioana Caramida", "c.caramida@gmail.com", bilet2);
-
-    std::cout << "\nPasageri creati:\n" << p1 << "\n" << p2 << "\n" << p3 << "\n";
-    std::cout << "Nume p2: " << p2.getNume() << "\n";
-    std::cout << "Bilet p3: " << p2.getBilet() << "\n";
-
-    Bilet biletNou("5C", "Economic", 200.3);
-    biletNou.aplicaDiscount(15);
-    p2.setBilet(biletNou);
-    std::cout <<"\nDupa discount:\n" << p2 << "\n";
-
-    //ZBOR
-    std:: cout << "\nTEST3: Functionalitate Zbor";
-
-    Zbor z1;
-    Zbor z2("RO123", "Paris", "A12", 3);
-    std::cout << "Zborurile: " << z1 << z2 << "\n";
-
-    std::cout <<"Numar zbor: " << z2.getNumarZbor() << "\n";
-    std::cout <<"Poarta: " << z2.getPoarta() << "\n";
-    std::cout <<"Locuri ocupate: " << z2.getLocuriOcupate() << "\n";
-    std::cout <<"Capacitate: " << z2.getCapacitateMaxima() << "\n";
-
-    z2.setPoarta("B15");
-    std::cout << "\nNoua poarta: " << z2.getPoarta() << "\n";
-    z2.setPoarta("123"); // eroare
-
-    std::cout << "\nZbor plin? " << (z2.isFull()? "DA" : "NU") << "\n";
-
-    //Noi pasageri
-    Bilet bz1("12A", "Economic", 120.7);
-    Bilet bz2("8F", "Business", 400.5);
-    bz2.aplicaDiscount(10);
-    Bilet bz3("8C", "Economic", 110.0);
-    Bilet bz4("22D", "Economic", 115.0);
-
-    Pasager pz1("Ana Popescu", "a.popescu@gmail.com", bz1);
-    Pasager pz2("Ion Aranutu", "arnautu@gmail.com", bz2);
-    Pasager pz3("Elena Dumitru", "dumitru@gmail.com", bz3);
-    Pasager pz4("Mihai Olteanu", "olteanu@gmail.com", bz4);
-
-    std::cout <<"\nAdaug pasageri...";
-    z2.adaugaPasager(pz1);
-    z2.adaugaPasager(pz2);
-    z2.adaugaPasager(pz3);
-
-    std::cout <<"\n" << z2 << "\n";
-
-    //incercam sa adaugam cand e plin
-    std::cout<<"Adaug cand e plin:\n";
-    z2.adaugaPasager(pz4);
-
-    //calculam incasari
-    std::cout   << "\nIncasari totale: " << std::fixed << std::setprecision(2)
-                << z2.calculeazaIncasariTotale() << " EUR\n";
-
-    //cautam pasager
-    Pasager* gasit = z2.cautaPasagerDupaNume("Ion Aranutu");
-    if (gasit) {
-        std::cout << "\nPasager gasit!\n" << *gasit << "\n";
+            Zbor *z = companie.cautaZborDupaNumar(numar);
+            if (z != NULL) {
+                std::cout << "\n" << *z << "\n";
+            } else {
+                std::cerr << "\nZbor negasit!\n";
+            }
+        }
     }
 
-    Pasager* negasit = z2.cautaPasagerDupaNume("Vasile Ion");
-    if (!negasit) {
-        std::cout << "\nPasagerul nu exita!\n" << *negasit << "\n";
-    }
-
-    //Companie aeriana
-    std:: cout << "\n!!!!!!!!!!!!!!!!TEST4: Functionalitate Companie";
-    CompanieAeriana Wizz("Wizz");
-    std::cout <<"\nCompanie: " << Wizz.getNumeCompanie() << "\n";
-
-    //cream zboruri
-    Zbor zb1("RO101", "Paris", "A12", 2);
-    Zbor zb2("RO202", "Londra", "B8", 3);
-    Zbor zb3("RO303", "Roma", "C15", 2);
-
-    std::cout<<"\nAdauga zboruri:...\n";
-    Wizz.adaugaZbor(zb1);
-    Wizz.adaugaZbor(zb2);
-    Wizz.adaugaZbor(zb3);
-
-    std::cout << "\n" << Wizz << "\n";
 
 }
